1d14a1626e1de740f0251c430834c971e19c3350
ZhangYanJiong <zhangyanjiong@163.com>
2019-02-26 18:38:43 +0800

    update copyright

--- api/modules/cgi-bin/components/Identity.php
+++ api/modules/cgi-bin/components/Identity.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link https://github.com/devzyj/yii2-application
- * @copyright Copyright (c) 2018 Zhang Yan Jiong
+ * @copyright Copyright (c) 2019 Zhang Yan Jiong
  * @license http://opensource.org/licenses/BSD-3-Clause
  */
 namespace apiCgiBin\components;
